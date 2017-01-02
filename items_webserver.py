4bdbf071d5be97e39d3ec49f5f1ab70ecb7c4fdd
ZEE CHEN <zeyuan.chen1126@gmail.com>
2017-01-01 19:31:58 -0800

    connect to database

--- items_webserver.py
+++ items_webserver.py
@@ -1,6 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+# import database functions
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from items_db_setup import Base, Catagory, Item
+
+# Create session and connect to DB
+engine = create_engine('sqlite:///catagories.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 
 
 
