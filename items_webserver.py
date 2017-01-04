002dadb33d2933552e41b697b83a9e4110debb24
ZEE CHEN <zeyuan.chen1126@gmail.com>
2017-01-03 19:15:02 -0800

    - link webserver to catagories database - link catalog page to individual catalog's items page and vice versa

--- items_webserver.py
+++ items_webserver.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from items_db_setup import Base, Catagories, Items
+from items_db_query import db_catagories, db_items
 
 # Create session and connect to DB
 engine = create_engine('sqlite:///catagories.db')
@@ -12,36 +13,39 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-catagory_id = 2
 
 
 #Fake catagory
-catagory = {'name': 'The CRUDdy Crab', 'id': '1'}
+# catagory = {'name': 'The CRUDdy Crab', 'id': '1'}
 
-catagories = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+# catagories = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
 
 
 #Fake catagory items
-items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','id':'3'},{'name':'Iced Tea', 'description':'with lemon','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','id':'5'} ]
-item =  {'name':'Cheese Pizza','description':'made with fresh cheese'}
+# items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','id':'3'},{'name':'Iced Tea', 'description':'with lemon','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','id':'5'} ]
+# item =  {'name':'Cheese Pizza','description':'made with fresh cheese'}
 
 
 
 
 
 @app.route('/')
-@app.route('/catagories/')
+@app.route('/catalog/')
 def allCatagories():
+	catagories=db_catagories(session)
 	return render_template('catagories.html', catagories = catagories)
 
 
 @app.route('/<string:catagory_name>/items')
 def allItems(catagory_name):
+	catagory = db_catagories(session)
+	items = db_items(session, catagory_name)
 	return render_template('items.html', items = items, catagory = catagory)
 
 
 @app.route('/<string:catagory_name>/item/new')
 def newItem(catagory_name):
+	catagory = db_catagory(session, catagory_name)
 	return render_template('newitem.html', catagory = catagory)
 
 
