89676e74ed8034a01df3676dcd080b4ac0d4bbd5
ZEE CHEN <zeyuan.chen1126@gmail.com>
2017-01-04 17:16:05 -0800

    google oauth login successful

--- items_webserver.py
+++ items_webserver.py
@@ -122,7 +122,45 @@ def gconnect():
     login_session['email'] = data['email']
 
     flash("you are now logged in as %s" % login_session['username'])
-    return
+    return ""
+
+
+    # DISCONNECT - Revoke a current user's token and reset their login_session
+
+
+@app.route('/gdisconnect')
+def gdisconnect():
+	access_token = login_session['access_token']
+	print 'In gdisconnect access token is %s', access_token
+	print 'User name is: ' 
+	print login_session['username']
+ 
+	if access_token is None:
+		print 'Access Token is None'
+		response = make_response(json.dumps('Current user not connected.'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+
+	url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+	h = httplib2.Http()
+	result = h.request(url, 'GET')[0]
+	print 'result is '
+	print result
+
+	if result['status'] == '200':
+		del login_session['credentials']
+		del login_session['access_token'] 
+		del login_session['gplus_id']
+		del login_session['username']
+		del login_session['email']
+		del login_session['picture']
+		response = make_response(json.dumps('Successfully disconnected.'), 200)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	else:
+		response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+		response.headers['Content-Type'] = 'application/json'
+		return response
 
 
 # JSON APIs for category and item info
